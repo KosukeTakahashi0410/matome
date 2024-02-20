f3b23a0affd3a679190e72e21b83851238b6f75f
takahashikosuke <takahashikosuke@team-lab.com>
2024-02-20 20:07:47 +0900

    :wrench: typo

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -11,7 +11,7 @@ const config: Config = {
   url: "https://kosuketakahashi0410.github.io/",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/matme",
+  baseUrl: "/matome",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
